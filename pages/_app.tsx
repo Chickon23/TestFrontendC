c07c667c1957050e10867c230683d900289749d1
GTO <giorgio.torre@stellenanzeigen.de>
2022-03-23 13:53:37 +0100

    redux store initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,91 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import App, { AppInitialProps } from "next/app";
+import { wrapper } from "../redux/store";
+// import { getConfig } from "../config/slices/configSlice";
+// import { createGlobalStyle } from "styled-components";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+class MyApp extends App<AppInitialProps> {
+  public static getInitialProps = wrapper.getInitialAppProps(
+    (store) => async (context) => {
+      // const globalConfig = await store.dispatch(getConfig());
+
+      return {
+        pageProps: {
+          ...(await App.getInitialProps(context)).pageProps,
+          // config: globalConfig,
+        },
+      };
+    }
+  );
+
+  public render() {
+    const { Component, pageProps } = this.props;
+    // const getLayout = Component.getLayout || ((page) => page);
+
+    // const { Color } = pageProps.config.payload;
+
+    // return getLayout(
+    //   <>
+    //     <GlobalStyle color={Color} />
+    //     <Component {...pageProps} />
+    //   </>
+    // );
+
+    return <Component {...pageProps} />;
+  }
 }
 
-export default MyApp
+// const GlobalStyle = createGlobalStyle`
+//   html, body {
+//     padding: 0;
+//     margin: 0;
+//     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+//   }
+
+//   a {
+//     color: inherit;
+//     text-decoration: none;
+//     cursor: pointer;
+//   }
+
+//   button {
+//     font-size: 24px;
+//     font-weight: 700;
+//     background-color: #ffffff;
+//     border-color: ${({ color }) => color};
+//     border-radius: 5px;
+//     cursor: pointer;
+//   }
+
+//   button:hover {
+//     background-color: ${({ color }) => color};
+//     color: #ffffff;
+//   }
+
+//   input {
+//     border-color: ${({ color }) => color};
+//     border-radius: 5px;
+//   }
+
+//   * {
+//     box-sizing: border-box;
+//     color: ${({ color }) => color};
+//   }
+
+//   /* width */
+//   ::-webkit-scrollbar {
+//     width: 6px;
+//   }
+
+//   /* Track */
+//   ::-webkit-scrollbar-track {
+//     background: #f1f1f1;
+//   }
+
+//   /* Handle */
+//   ::-webkit-scrollbar-thumb {
+//     background: ${({ color }) => color};
+//     border-radius: 5px;
+//   }
+//`;
+
+export default wrapper.withRedux(MyApp);
---
+++ redux/store.ts
@@ -0,0 +1,31 @@
+import { createWrapper } from "next-redux-wrapper";
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+
+// import { configReducer } from "../config/slices/configSlice";
+// import { landingpageReducer } from "../landingPage/slices/landingpageSlice";
+// import { stelrFullTextReducer } from "../search/slices/stelrFullTextSlice";
+// import { stelrFullTextOffsetReducer } from "../jobs/slices/stelrFullTextOffsetSlice";
+// import { stelrIdReducer } from "../search/slices/stelrIdSlice";
+
+const makeStore = () =>
+  configureStore({
+    reducer: {
+      //   [configReducer.name]: configReducer.reducer,
+      //   [landingpageReducer.name]: landingpageReducer.reducer,
+      //   [stelrFullTextReducer.name]: stelrFullTextReducer.reducer,
+      //   [stelrFullTextOffsetReducer.name]: stelrFullTextOffsetReducer.reducer,
+      //   [stelrIdReducer.name]: stelrIdReducer.reducer,
+    },
+    devTools: true,
+  });
+
+export const wrapper = createWrapper(makeStore, { debug: true });
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type AppState = ReturnType<AppStore["getState"]>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action
+>;
