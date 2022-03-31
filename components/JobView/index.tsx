bd5a9cf422758ef7f41f91c451698ae8806d4f35
TommyH23 <t.heft@yourfirm.de>
2022-03-31 08:22:24 +0200

    Adjust text to avoid escape issues

--- components/JobView/index.tsx
+++ components/JobView/index.tsx
@@ -37,7 +37,7 @@ const JobView = ({
         <img src={logoUrl} />
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industries standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
@@ -54,29 +54,29 @@ const JobView = ({
           Latin words, consectetur, from a Lorem Ipsum passage, and going
           through the cites of the word in classical literature, discovered the
           undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+          1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and
           Evil) by Cicero, written in 45 BC. This book is a treatise on the
           theory of ethics, very popular during the Renaissance. The first line
-          of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
+          of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in
           section 1.10.32.
         </p>
         <p>
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
+          letters, as opposed to using Content here, content here, making it
           look like readable English. Many desktop publishing packages and web
           page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
+          search for orem ipsum will uncover many web sites still in their
           infancy. Various versions have evolved over the years, sometimes by
           accident, sometimes on purpose (injected humour and the like).
         </p>
         <p>
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
-          humour, or randomised words which don't look even slightly believable.
+          humour, or randomised words which dont look even slightly believable.
           If you are going to use a passage of Lorem Ipsum, you need to be sure
-          there isn't anything embarrassing hidden in the middle of text. All
+          there isnt anything embarrassing hidden in the middle of text. All
           the Lorem Ipsum generators on the Internet tend to repeat predefined
           chunks as necessary, making this the first true generator on the
           Internet. It uses a dictionary of over 200 Latin words, combined with
